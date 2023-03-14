9a6191d41d8708563bb6cde23710ec6e1318a444
Shumba07 <lionelchisango@icloud.com>
2023-03-14 03:41:19 -0700

    100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -7,7 +7,6 @@
  * @av: double pointer array
 * Return: 0
 */
-
 char *argstostr(int ac, char **av)
 {
 	int i, n, r = 0, l = 0;
@@ -17,7 +16,6 @@ char *argstostr(int ac, char **av)
 		return (NULL);
 
 	for (i = 0; i < ac; i++)
-
 	{
 
 		for (n = 0; av[i][n]; n++)
@@ -30,27 +28,20 @@ char *argstostr(int ac, char **av)
 
 	return (NULL);
 	for (i = 0; i < ac; i++)
-
 	{
 
 	for (n = 0; av[i][n]; n++)
-
 	{
-
 	str[r] = av[i][n];
 	r++;
 	}
 
 	if (str[r] == '\0')
-
 	{
-
 	str[r++] = '\n';
 
 	}
-
 	}
-
 	return (str);
 
 }
