0db73fcaa2b00c7006a4bb886d141ca86782f05c
Shumba07 <lionelchisango@icloud.com>
2023-03-22 13:45:52 -0700

    A goal is not always meant to be reached, it often serves simply as something to aim at

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,13 +1,14 @@
 #include "3-calc.h"
 #include <stdlib.h>
+#include <string.h>
+
+
 /**
- * get_op_func - Selects the correct function to perform
- * the operation asked by the user.
- * @s: The operator passed as argument.
- *
- * Return: A pointer to the function corresponding
- * to the operator given as a parameter.
- */
+* get_op_func - ...
+* @s: ...
+*
+* Return: ...
+*/
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
@@ -16,17 +17,20 @@ int (*get_op_func(char *s))(int, int)
 	{"*", op_mul},
 	{"/", op_div},
 	{"%", op_mod},
-	{NULL, NULL},
-	};
-
-
+	{NULL, NULL}};
 	int i = 0;
 
 
-	while (ops[i].op != NULL && *(ops[i].op) != *s)
-	i++;
-
-
+	while (i < 5)
+	{
+	if (strcmp(s, ops[i].op) == 0)
 	return (ops[i].f);
+
+
+	i++;
+	}
+
+
+	return (0);
 }
 
